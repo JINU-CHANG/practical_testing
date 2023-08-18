286081929b384f0139bd5f0b4de439e691aefa91
JINU-CHANG <jenny010925@naver.com>
2023-08-18 16:16:47 +0900

    parameterized test 연습

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -2,6 +2,8 @@ package sample.cafekiosk.spring.domain.product;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,14 +11,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProductTypeTest {
 
     @DisplayName("상품 타입이 재고 관련 타입인지 확인한다.")
-    @Test
-    void containsStockType() {
-        // given
-        ProductType givenType = ProductType.HANDMADE;
+    @CsvSource({"HANDMADE, false", "BOTTLE, true", "BAKERY, true"})
+    @ParameterizedTest
+    void containsStockType(ProductType productType, boolean expected) {
         // when
-        boolean result = ProductType.containsStockType(givenType);
+        boolean result = ProductType.containsStockType(productType);
         // then
-        assertThat(result).isFalse();
+        assertThat(result).isEqualTo(expected);
     }
 
 }
